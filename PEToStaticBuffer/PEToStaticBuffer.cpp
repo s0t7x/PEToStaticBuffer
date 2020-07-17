bd3071265fa0320e5eaa6c749faff148d99c5c5c
Miguel Oppermann <miguel.o@gmx.net>
2020-07-17 20:34:17 +0200

    optimized readability in output. Payed with readability in code....

--- PEToStaticBuffer/PEToStaticBuffer.cpp
+++ PEToStaticBuffer/PEToStaticBuffer.cpp
@@ -1,6 +1,8 @@
 // PE to static buffer
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <iomanip>
 #include <ctime>
 
 int main(int argC, char * argV[]) {
@@ -11,6 +13,7 @@ int main(int argC, char * argV[]) {
 
 	std::ifstream pEFile;
 	pEFile.open(argV[1]);
+
 	if (pEFile) {
 		pEFile.seekg(0, pEFile.end);
 		int pEBytes = (int)pEFile.tellg();
@@ -21,19 +24,23 @@ int main(int argC, char * argV[]) {
 
 		std::time_t timeStamp = std::time(NULL);
 
-		#pragma warning (disable: 4996)
+#pragma warning (disable: 4996)
 		std::cout << "//PEStaticBuffer of \"" << argV[1] << "\"\n//Generated on " << std::asctime(std::localtime(&timeStamp)) << std::endl;
 		std::cout << "#pragma once" << std::endl << std::endl;
 
 		std::cout << "STATIC UINT8 PE_BUFFER[] = {" << std::endl;
 
 		for (int i = 0; i < (pEBytes - 1); i++) {
-			std::cout << "0x" << std::hex << (int)(uint8_t)pEBuffer[i] << ", ";
+			std::cout << "0x" << std::setfill('0') << std::setw(2) << std::hex << std::right << std::uppercase << (int)(uint8_t)pEBuffer[i] << ", ";
+			if (i % 16 == 15)
+				std::cout << std::endl;
 		}
-		std::cout << "0x" << std::hex << (int)(uint8_t)pEBuffer[pEBytes - 1] << std::endl;
+
+		std::cout << "0x" << std::setfill('0') << std::setw(2) << std::hex << std::right << (int)(uint8_t)pEBuffer[pEBytes - 1] << std::endl;
+
 
 		std::cout << "};" << std::endl;
-	}
 
-	return 0;
+		return 0;
+	}
 }
